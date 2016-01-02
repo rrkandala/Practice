591e4ead306a9732659c485c249f68a37719c2e1
Ankit Srivastava <ankit.ufl@gmail.com>
2016-01-01 17:52:05 -0800

    Cat and Dog Shelter Queue

--- CrackingTheCodeInterview/3.7/CatDogShelterQueue.cs
+++ CrackingTheCodeInterview/3.7/CatDogShelterQueue.cs
@@ -3,64 +3,85 @@
  *	
  *	Date: 1/1/2016
  */
- 
+
 namespace CrackingTheCodingInterview
-{	
-	public class CatDogShelterQueue
-	{
-		internal CatDog
+{
+    /// <summary>
+    /// Class for a cat and dog shelter queue
+    /// </summary>
+    public class CatDogShelterQueue
+    {
+        /// <summary>
+        /// Class for storing type and number of cat/dog
+        /// </summary>
+        public class CatDog
 		{
 			private CatDogType type;
-			
-			private int num;
-			
-			/// <summary>
-			/// Constructor for initializing CatDog with its type and number
-			/// </summary>
-			public CatDog(int n, CatDogType t)
-			{
-				num = n;
-				type = t;
-			}
-		}
-		
-		private SinglyLinkedList queue;
-		
-		/// <summary>
-		/// Constructor initializing a singly linked list acting as a FIFO queue
-		/// </summary>
-		public CatDogShelterQueue()
-		{
-			queue = new SinglyLinkedList();
-		}
-		
-		/// <summary>
-		/// <param name="d"> Cat/Dog Number </param>
-		/// <param name="c"> Type - Cat or Dog </param>
-		/// </summary>
-		public void enqueue(object d, CatDogType c)
-		{
-			CatDog cd = new CatDog(d, c);
-			
-			queue.Insert(cd);
-		}
-		
-		public CatDog dequeueAny()
-		{
-			return (CatDog)queue.DeleteEnd();
-		}
-	}
-	
-	internal class CatDogShelterQueueTest
-	{
-		static void Main()
-		{
-			
-		}
-	}
-	
-	enum CatDogType
-	{
-		Cat, Dog
-	};
+
+            private int num;
+
+            /// <summary>
+            /// Constructor for initializing CatDog with its type and number
+            /// </summary>
+            public CatDog(int n, CatDogType t)
+            {
+                num = n;
+                type = t;
+            }
+        }
+
+        private SinglyLinkedList queue;
+
+        /// <summary>
+        /// Constructor initializing a singly linked list acting as a FIFO queue
+        /// </summary>
+        public CatDogShelterQueue()
+        {
+            queue = new SinglyLinkedList();
+        }
+
+        /// <summary>
+        /// <param name="d"> Cat/Dog Number </param>
+        /// <param name="c"> Type - Cat or Dog </param>
+        /// </summary>
+        public void enqueue(object d, CatDogType c)
+        {
+            CatDog cd = new CatDog((int)d, c);
+
+            queue.Insert(cd);
+        }
+
+        /// <summary>
+        /// Dequeueing last animal - cat or dog
+        /// </summary>
+        /// <returns>Cat or Dog oldest arrival</returns>
+        public CatDog dequeueAny()
+        {
+            return (CatDog)queue.DeleteEnd();
+        }
+    }
+
+    internal class CatDogShelterQueueTest
+    {
+        static void Main()
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// Cat or Dog
+    /// </summary>
+    public enum CatDogType
+    {
+        /// <summary>
+        /// Cat
+        /// </summary>
+        Cat,
+
+        /// <summary>
+        /// Dog
+        /// </summary>
+        Dog
+    };
 }
