f51e28a2c8b34cab9d03e22dc563a7d971bebc1e
Ankit Srivastava <ankit.ufl@gmail.com>
2016-01-09 01:35:03 -0800

    Binary Tree updates

--- CrackingTheCodeInterview/4.1/CheckBalancedTree.cs
+++ CrackingTheCodeInterview/4.1/CheckBalancedTree.cs
@@ -14,6 +14,7 @@ namespace CrackingTheCodingInterview
     public class TreeNode
     {
         private object data;
+		private int depth;
         private TreeNode left;
         private TreeNode right;
 
@@ -63,13 +64,29 @@ namespace CrackingTheCodingInterview
             }
         }
 
+		/// <summary>
+		/// Depth of tree
+		/// </summary>
+		public int Depth
+		{
+			get
+			{
+				return depth;
+			}
+			set
+			{
+				depth = value;
+			}
+		}
+
         /// <summary>
         /// Constructor initializing the current node
         /// </summary>
-        /// <param name="d"></param>
+        /// <param name="d">Data element</param>
         public TreeNode(object d)
         {
             Data = d;
+			Depth = 1;
             Left = null;
             Right = null;
         }
@@ -78,7 +95,7 @@ namespace CrackingTheCodingInterview
     /// <summary>
     /// Binary Tree Implementation
     /// </summary>
-    public class BTree
+    public partial class BTree
     {
         private TreeNode root;
 
@@ -88,12 +105,13 @@ namespace CrackingTheCodingInterview
         public BTree()
         {
             root = null;
+			linkedLists = new List<SinglyLinkedList>();
         }
 
         /// <summary>
         /// Inserting value in tree
         /// </summary>
-        /// <param name="d"></param>
+        /// <param name="d">Data Element</param>
         public void Insert(object d)
         {
             if (root == null)
@@ -109,8 +127,8 @@ namespace CrackingTheCodingInterview
         /// <summary>
         /// Recursively insert value in tree
         /// </summary>
-        /// <param name="d"></param>
-        /// <param name="leaf"></param>
+        /// <param name="d">Data element</param>
+        /// <param name="leaf">Current root node</param>
         public void Insert(object d, TreeNode leaf)
         {
             if ((int)d < (int)leaf.Data)
@@ -140,7 +158,7 @@ namespace CrackingTheCodingInterview
         /// <summary>
         /// Search a value in tree
         /// </summary>
-        /// <param name="d"></param>
+        /// <param name="d">Data element</param>
         /// <returns>true if value found else false</returns>
         public bool Search(object d)
         {
