2c33a388af3db3da8e2df190ed2d178946a21de7
Ankit <ankit.ufl@gmail.com>
2016-12-07 21:41:29 -0800

    kStacks using an array
    free, top[k], next[n]

--- CrackingTheCodeInterview/3.1/ThreeStacks.cpp
+++ CrackingTheCodeInterview/3.1/ThreeStacks.cpp
@@ -7,6 +7,7 @@ class ThreeStacks
 private:
 	int *a;
 	int n;
+	int free;
 	int *top;
 	int *next;
 public:
@@ -16,6 +17,7 @@ public:
 	int Pop(int i);
 	bool IsEmpty(int i);
 	int Peek(int i);
+	bool IsFull(int i);
 };
 
 ThreeStacks::ThreeStacks()
@@ -28,25 +30,79 @@ ThreeStacks::ThreeStacks(int size)
 	n = size;
 	a = new int[n];
 	top = new int[3];
-	next = new int[3];
+	next = new int[n];
 
 	for (int i = 0; i < 3; i++)
 	{
-		top[i] = i;
-		next[i] = i + 3;
+		top[i] = -1;
 	}
+
+	free = 0;
+	for (int i = 0; i < n; i++)
+	{
+		next[i] = i + 1;
+	}
+
+	next[n - 1] = -1;	// -1 is used to indicate end of free list
 }
 
-void ThreeStacks::Push(int i, int d)
+bool ThreeStacks::IsFull(int i)
 {
-	if (i > 3)
+	return free == -1;
+}
+
+bool ThreeStacks::IsEmpty(int sn)
+{
+	return top[sn] == -1;
+}
+
+int ThreeStacks::Peek(int sn)
+{
+	return a[top[sn]];
+}
+
+void ThreeStacks::Push(int sn, int d)
+{
+	if (sn > 3)
 	{
-		cout << "Stack number: " << i << " is greater than number of stacks = 3";
+		cout << "Stack number: " << sn << " is greater than number of stacks = 3";
 		return;
 	}
 	else
 	{
-		a[next[i-1]] = d;
+		if (IsFull(sn))
+		{
+			cout << "Stack " << sn << " is full";
+		}
+		else
+		{
+			int i = free;
+			// update index of free slot to index of next slot in free list
+			free = next[i];
+			next[i] = top[sn];
+			top[sn] = i;
+
+			// Put item in array
+			a[i] = d;
+		}
+	}
+}
+
+int ThreeStacks::Pop(int sn)
+{
+	if (IsEmpty(sn))
+	{
+		cout << "Stack " << sn << " is empty";
+		return -1;
+	}
+	else
+	{
+		int i = top[sn];
+		top[sn] = next[i];
+		next[i] = free;
+		free = i;
+
+		return a[i];
 	}
 }
 
@@ -56,7 +112,26 @@ int main()
 	cout << "Enter size of array to implement 3 stacks: ";
 	cin >> n;
 
+	ThreeStacks t(n);
 
+	t.Push(0, 1);
+	t.Push(0, 2);
+	t.Push(0, 3);
+	t.Push(0, 4);
+	t.Push(1, 5);
+	t.Push(2, 6);
+	t.Push(1, 7);
+	t.Push(1, 8);
+
+	cout << "Pop: " << t.Pop(0) << endl;
+	cout << "Pop: " << t.Pop(0) << endl;
+	cout << "Pop: " << t.Pop(0) << endl;
+	cout << "Pop: " << t.Pop(0) << endl;
+	cout << "Pop: " << t.Pop(0) << endl;
+	cout << "Pop: " << t.Pop(1) << endl;
+	cout << "Pop: " << t.Pop(1) << endl;
+	cout << "Pop: " << t.Pop(1) << endl;
+	cout << "Pop: " << t.Pop(2) << endl;
 
 	return 0;
 }
